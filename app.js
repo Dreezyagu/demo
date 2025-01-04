235360d0bdc39e70c6e9c884c46630cde3cc062a
Ifeanyi Agu <aaifeanyi20@outlook.com>
2025-01-04 18:19:46 +0200

    added mongoose to project

--- app.js
+++ app.js
@@ -2,38 +2,67 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://ifeanyi-driver:kTssAnhPr5MnJeG1@demo-cluster.d84qr.mongodb.net/shop?w=majority&appName=demo-cluster';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+  });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: "my secret", resave: false, saveUninitialized: false, store: store}));
 
 app.use((req, res, next) => {
-  User.findById('67768f413fffb6ac4e5f484c')
+  User.findById('6778043e689bbac281ed5022')
   .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err));
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
-
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
-
+mongoose
+  .connect(
+    MONGODB_URI
+  )
+  .then(result => {
+    
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Ifeanyi',
+          email: 'ifeanyi@yopmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+    }
+  });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
